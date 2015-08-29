6ea13abfb4dc52a0ed9f327a2142acd30b10eadd
Damian <visualfanatic@users.noreply.github.com>
2015-08-30 01:49:13 +0200

    Update a year

--- SteamAccountSwitcher/Properties/AssemblyInfo.cs
+++ SteamAccountSwitcher/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SteamAccountSwitcher")]
-[assembly: AssemblyCopyright("Copyright © Christoph Wedenig 2014")]
+[assembly: AssemblyCopyright("Copyright © Christoph Wedenig 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
